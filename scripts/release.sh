93d6f922f5ddba1a97c195eddbd7f805a980eb2c
Joel Griffith <jgriffith@appnexus.com>
2015-05-21 12:53:09 -0700

    Fixing release script, new release file :)

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,8 +1,13 @@
 # Remove any references to Scriptin99 in package.json
-cat package.json | 
-sed 's/Scriptin99/YourProject/g' | 
-sed 's/jgriffith/YourName/g' |
-sed 's/APACHE V2/YourLicense/g' > package.json
+#! /bin/bash
+sed -i ''\
+    -e 's/APACHE V2/Your License/g'\
+    -e 's/Scriptin99/Your Project/g'\
+    -e 's/jgriffith/Your Name/g'\
+    ./package.json
 
-# Tar up the directory, load our exclusions
-tar -zcvf latest.tar.gz -X ./conf/archive.exclude.txt ./ && git checkout package.json
+# Tar up the directory, ignore unwanted files
+tar -zcvf latest.tar.gz -X ./conf/archive.exclude.txt ./
+
+# Reset the directory
+git checkout package.json
